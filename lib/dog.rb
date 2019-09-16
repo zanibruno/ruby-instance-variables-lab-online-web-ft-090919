84ee424222485eebf357eef502abec8af5b29fac
Bruno de Oliveira <corporate.zanin@gmail.com>
2019-09-16 15:07:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog 
- @this_dogs_name = dog_name
+ lassie = Dog.new 
+ lassie.name = "Lassie"
+ lassie.name
+ 
+ def name=(dog_name)
+   this_dogs_name = dog_name
+ end 
+ 
+ def name 
+   this_dogs_name
+ end
 end
